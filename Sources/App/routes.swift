461a8dbe98e515763316fe468b39892bfbb6157c
Will McGinty <mcgintw@gmail.com>
2018-07-21 14:02:23 -0500

    Create POST /movies to add a movie

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,4 +11,8 @@ public func routes(_ router: Router) throws {
     router.get("movies") { req -> Future<[Movie]> in
         return Movie.query(on: req).all()
     }
+    
+    router.post(Movie.self, at: "movies") { req, movie -> Future<HTTPResponseStatus> in
+        return movie.save(on: req).transform(to: .created)
+    }
 }
